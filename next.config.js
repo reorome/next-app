0da9b411af49535a28e32254eaef3da6a395b146
reorome <with.ot24@gmail.com>
2021-08-01 07:12:38 +0000

    init

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 200,
+      };
+    }
+
+    return config;
+  },
+}
